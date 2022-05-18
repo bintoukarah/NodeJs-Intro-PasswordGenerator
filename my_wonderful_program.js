f3f41045d20f3ef2966105b10eb34c428a32b486
bkaramo <bintoukaramoko3@gmail.com>
2022-05-18 14:14:29 +0200

    added password function

--- my_wonderful_program.js
+++ my_wonderful_program.js
@@ -1,1 +1,25 @@
-console.log("Hello world");
+import clipboard from 'clipboardy';
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+console.log(makeid(5));
+
+// let result = '';
+// const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+// let charactersLength = characters.length;
+
+// makePassword('length', () => {
+//     for (let i = 0; i < length; index++) {
+//         result += characters.charAt(Math.floor(Math.random()
+//         * charactersLength));
+//     }
+// })
+// console.log(makePassword(5));
